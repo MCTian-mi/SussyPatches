71b21fa2acf7b48853d82ff3d6ea744b2e4389a1
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-08-07 14:40:37 +0800

    :spotless:

--- src/main/java/dev/tianmi/sussypatches/common/CommonProxy.java
+++ src/main/java/dev/tianmi/sussypatches/common/CommonProxy.java
@@ -1,11 +1,12 @@
 package dev.tianmi.sussypatches.common;
 
-import com.cleanroommc.modularui.factory.GuiManager;
-import dev.tianmi.sussypatches.api.metatileentity.mui2.MTEGuiFactory;
-import dev.tianmi.sussypatches.api.util.SusMods;
 import net.minecraftforge.fml.common.Mod;
 
+import com.cleanroommc.modularui.factory.GuiManager;
+
 import dev.tianmi.sussypatches.Tags;
+import dev.tianmi.sussypatches.api.metatileentity.mui2.MTEGuiFactory;
+import dev.tianmi.sussypatches.api.util.SusMods;
 
 @Mod.EventBusSubscriber(modid = Tags.MODID)
 public class CommonProxy {
