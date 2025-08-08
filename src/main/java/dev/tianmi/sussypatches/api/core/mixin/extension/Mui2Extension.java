70527adec0c2a2cc991f999f7ba5eea5ed34fd46
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-08-08 20:53:42 +0800

    fix: better mui2 api

--- src/main/java/dev/tianmi/sussypatches/api/core/mixin/extension/Mui2Extension.java
+++ src/main/java/dev/tianmi/sussypatches/api/core/mixin/extension/Mui2Extension.java
@@ -1,26 +1,25 @@
 package dev.tianmi.sussypatches.api.core.mixin.extension;
 
+import com.cleanroommc.modularui.api.IGuiHolder;
 import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 
 import dev.tianmi.sussypatches.api.annotation.MixinExtension;
-import dev.tianmi.sussypatches.api.metatileentity.mui2.IMui2Holder;
 import gregtech.api.metatileentity.MetaTileEntity;
 
-/// A default impl of [IMui2Holder] for the base MTE class
+/// A default impl of [IGuiHolder] for the base MTE class
 /// DO NOT implement this for your mte directly!
 /// Use [IMui2Holder] instead!
 @MixinExtension(MetaTileEntity.class)
-public interface Mui2Extension extends IMui2Holder {
+public interface Mui2Extension extends IGuiHolder<PosGuiData> {
 
     @Override
     default ModularPanel buildUI(PosGuiData guiData, PanelSyncManager syncManager, UISettings settings) {
         return null;
     }
 
-    @Override
     default boolean useMui2() {
         return false;
     }
--- src/main/java/dev/tianmi/sussypatches/api/metatileentity/mui2/IMui2Holder.java
+++ src/main/java/dev/tianmi/sussypatches/api/metatileentity/mui2/IMui2Holder.java
@@ -1,16 +1,21 @@
 package dev.tianmi.sussypatches.api.metatileentity.mui2;
 
-import com.cleanroommc.modularui.api.IGuiHolder;
+import org.jetbrains.annotations.ApiStatus;
+
 import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.UISettings;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 
-public interface IMui2Holder extends IGuiHolder<PosGuiData> {
+import dev.tianmi.sussypatches.api.core.mixin.extension.Mui2Extension;
+
+@ApiStatus.AvailableSince("0.6.0")
+public interface IMui2Holder extends Mui2Extension {
 
     @Override
     ModularPanel buildUI(PosGuiData guiData, PanelSyncManager syncManager, UISettings settings);
 
+    @Override
     default boolean useMui2() {
         return true;
     }
