01061e5e8639fe9d0587da399ac7aa5c53575d91
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-08-15 22:07:41 +0800

    chore: relocate classes

--- src/main/java/dev/tianmi/sussypatches/core/MixinConfigPlugin.java
+++ src/main/java/dev/tianmi/sussypatches/core/MixinConfigPlugin.java
@@ -4,17 +4,12 @@ import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import dev.tianmi.sussypatches.api.core.IMixinConfigPlugin;
-import dev.tianmi.sussypatches.api.core.SusMixinTransformer;
+import dev.tianmi.sussypatches.core.asm.SusTransformers;
 
 public class MixinConfigPlugin implements IMixinConfigPlugin {
 
     @Override
     public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        SusMixinTransformer.preApply(targetClassName, targetClass, mixinClassName, mixinInfo);
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        SusMixinTransformer.postApply(targetClassName, targetClass, mixinClassName, mixinInfo);
+        SusTransformers.transform(targetClassName, targetClass);
     }
 }
---
+++ src/main/java/dev/tianmi/sussypatches/core/asm/SusTransformers.java
@@ -0,0 +1,29 @@
+package dev.tianmi.sussypatches.core.asm;
+
+import org.objectweb.asm.tree.ClassNode;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+
+import dev.tianmi.sussypatches.api.core.asm.IExplicitTransformer;
+import dev.tianmi.sussypatches.core.asm.transformer.MBPRTransformer;
+
+public class SusTransformers {
+
+    @SuppressWarnings("UnstableApiUsage")
+    private static final Multimap<String, IExplicitTransformer> TRANSFORMERS = MultimapBuilder.hashKeys()
+            .arrayListValues()
+            .build();
+
+    static {
+        add(new MBPRTransformer());
+    }
+
+    public static void transform(String targetClassName, ClassNode targetClass) {
+        TRANSFORMERS.get(targetClassName).forEach(transformer -> transformer.transform(targetClass));
+    }
+
+    private static void add(IExplicitTransformer transformer) {
+        TRANSFORMERS.put(transformer.targetClassName(), transformer);
+    }
+}
