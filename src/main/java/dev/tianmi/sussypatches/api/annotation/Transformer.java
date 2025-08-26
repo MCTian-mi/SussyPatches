f5cf5b319ec84f1edd39840c6d49950cbd785ddb
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-08-26 12:12:06 +0800

    format: smol cleanup

--- src/main/java/dev/tianmi/sussypatches/api/annotation/Transformer.java
+++ src/main/java/dev/tianmi/sussypatches/api/annotation/Transformer.java
@@ -5,13 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/// For dummy mixins that have corresponding [IExplicitTransformer]s
+/// For mixins that have corresponding [IExplicitTransformer]s
 /// And for transformers for their targets
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface Transformer {
 
-    /// The transformer class for this dummy mixin
+    /// The transformer class for this mixin
     Class<?>[] clazz() default {};
 
     /// The target class for this transformer
