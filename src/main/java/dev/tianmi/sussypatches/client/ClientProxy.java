6137c6f4d0d0f86b587b6d209da46d827860321a
bruberu <80226372+bruberu@users.noreply.github.com>
2025-08-26 23:23:11 -0500

    fix: undo client proxy edits

--- src/main/java/dev/tianmi/sussypatches/client/ClientProxy.java
+++ src/main/java/dev/tianmi/sussypatches/client/ClientProxy.java
@@ -1,14 +1,5 @@
 package dev.tianmi.sussypatches.client;
 
-import com.cleanroommc.modularui.api.drawable.IDrawable;
-import com.cleanroommc.modularui.drawable.GuiTextures;
-import com.cleanroommc.modularui.overlay.OverlayHandler;
-import com.cleanroommc.modularui.overlay.OverlayManager;
-import com.cleanroommc.modularui.screen.CustomModularScreen;
-import com.cleanroommc.modularui.screen.ModularPanel;
-import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
-import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -19,5 +10,4 @@ import dev.tianmi.sussypatches.common.CommonProxy;
 @SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(modid = Tags.MODID, value = Side.CLIENT)
 public class ClientProxy extends CommonProxy {
-
 }
