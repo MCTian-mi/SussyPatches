402bad3481a0c994cb0d0f70c38ef854d398f874
bruberu <80226372+bruberu@users.noreply.github.com>
2025-08-26 21:38:43 -0500

    feat: recipe creator port

--- src/main/java/dev/tianmi/sussypatches/client/ClientProxy.java
+++ src/main/java/dev/tianmi/sussypatches/client/ClientProxy.java
@@ -1,5 +1,14 @@
 package dev.tianmi.sussypatches.client;
 
+import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.overlay.OverlayHandler;
+import com.cleanroommc.modularui.overlay.OverlayManager;
+import com.cleanroommc.modularui.screen.CustomModularScreen;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -9,4 +18,6 @@ import dev.tianmi.sussypatches.common.CommonProxy;
 
 @SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(modid = Tags.MODID, value = Side.CLIENT)
-public class ClientProxy extends CommonProxy {}
+public class ClientProxy extends CommonProxy {
+
+}
