91e37379cd903c2b78070aeac744846541cbe79e
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-09-24 12:32:11 +0800

    fix: update info method signature to return generic type

--- src/main/java/dev/tianmi/sussypatches/integration/grs/GroovyExpansions.java
+++ src/main/java/dev/tianmi/sussypatches/integration/grs/GroovyExpansions.java
@@ -5,7 +5,7 @@ import gregtech.api.recipes.RecipeBuilder;
 
 public final class GroovyExpansions {
 
-    public static <R extends RecipeBuilder<R>> RecipeBuilder<R> info(RecipeBuilder<R> builder, String translationKey,
+    public static <R extends RecipeBuilder<R>> R info(R builder, String translationKey,
                                                                      Object... args) {
         builder.applyProperty(InfoProperty.getInstance(), new InfoProperty.TranslationData(translationKey, args));
         return builder;
