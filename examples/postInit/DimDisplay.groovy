0090361fd39cceb451d4e81535fd7a37c77ff064
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-10-12 00:24:51 +0800

    feat: add examples

---
+++ examples/postInit/DimDisplay.groovy
@@ -0,0 +1,16 @@
+package postInit
+
+import dev.tianmi.sussypatches.common.helper.DimDisplayRegistry
+
+
+DimDisplayRegistry.setDisplayItem(0, metaitem('supersymmetry:display.overworld'))
+DimDisplayRegistry.setDisplayItem(-1, metaitem('supersymmetry:display.nether'))
+DimDisplayRegistry.setDisplayItem(1, metaitem('supersymmetry:display.end'))
+DimDisplayRegistry.setDisplayItem(3, metaitem('supersymmetry:display.moon'))
+DimDisplayRegistry.setDisplayItem(400, metaitem('supersymmetry:display.titan'))
+DimDisplayRegistry.setDisplayItem(401, metaitem('supersymmetry:display.pluto'))
+DimDisplayRegistry.setDisplayItem(402, metaitem('supersymmetry:display.phobos'))
+DimDisplayRegistry.setDisplayItem(403, metaitem('supersymmetry:display.mercury'))
+DimDisplayRegistry.setDisplayItem(404, metaitem('supersymmetry:display.deep_dark'))
+DimDisplayRegistry.setDisplayItem(405, metaitem('supersymmetry:display.mars'))
+DimDisplayRegistry.setDisplayItem(406, metaitem('supersymmetry:display.deimos'))
