f9ae154d0286b2b748ae4f758fe05a55bbd12381
NepJr <diehardsteam@gmail.com>
2026-09-04 09:52:29 -0700

    I think I fixed config syncing

--- src/main/java/dev/tianmi/sussypatches/common/CommonProxy.java
+++ src/main/java/dev/tianmi/sussypatches/common/CommonProxy.java
@@ -1,7 +1,11 @@
 package dev.tianmi.sussypatches.common;
 
 import dev.tianmi.sussypatches.Tags;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.config.Config.Type;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = Tags.MOD_ID)
 public class CommonProxy {
@@ -11,4 +15,13 @@ public class CommonProxy {
     public void onInit() {}
 
     public void onPostInit() {}
+    
+    @SubscribeEvent
+    public static void syncConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+    	if(event.getModID().equals(Tags.MOD_ID))
+    	{
+    		ConfigManager.sync(Tags.MOD_ID, Type.INSTANCE);
+    	}
+    }
 }
