825e18d1e29a3e422adf8bf9539c1a88098eb97a
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2026-09-18 20:14:45 +0800

    fix: damn you mixinbooter

--- src/main/java/dev/tianmi/sussypatches/api/event/RenderItemOverlayEvent.java
+++ src/main/java/dev/tianmi/sussypatches/api/event/RenderItemOverlayEvent.java
@@ -1,14 +1,19 @@
 package dev.tianmi.sussypatches.api.event;
 
 import lombok.Getter;
+import lombok.SneakyThrows;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import org.jspecify.annotations.NullMarked;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 
 @Getter
 @NullMarked
@@ -18,10 +23,23 @@ public class RenderItemOverlayEvent extends Event {
 
     public static final RenderItemOverlayEvent INSTANCE = new RenderItemOverlayEvent();
 
+    private static final MethodHandle PHASE_SETTER = initPhaseSetter();
+
     protected RenderOperation operation = RenderOperation.EMPTY;
 
     @ApiStatus.Internal
     public RenderItemOverlayEvent() {
+    }
+
+    @SneakyThrows
+    private static MethodHandle initPhaseSetter() {
+        return MethodHandles.privateLookupIn(Event.class, MethodHandles.lookup())
+                .findSetter(Event.class, "phase", EventPriority.class);
+    }
+
+    @SneakyThrows
+    private void resetPhase() {
+        PHASE_SETTER.invokeExact((Event) this, (EventPriority) null);
     }
 
     public void enqueue(RenderOperation operation) {
@@ -30,7 +48,7 @@ public class RenderItemOverlayEvent extends Event {
 
     public void reset() {
         this.operation = RenderOperation.EMPTY;
-        this.phase = null;
+        resetPhase();
     }
 
     @Override
@@ -42,7 +60,8 @@ public class RenderItemOverlayEvent extends Event {
     public interface RenderOperation {
 
         // Do nothing, basically.
-        RenderOperation EMPTY = (stack, x, y, text) -> {};
+        RenderOperation EMPTY = (_, _, _, _) -> {
+        };
 
         void render(ItemStack stack, int x, int y, @Nullable String text);
 
